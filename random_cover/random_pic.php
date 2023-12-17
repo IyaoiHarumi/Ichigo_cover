843fc11f745a9b9b7277a49df04f856c5055c421
IyaoiHarumi <116329912+IyaoiHarumi@users.noreply.github.com>
2023-12-17 19:34:34 +0800

    Update random_pic.php

--- random_cover/random_pic.php
+++ random_cover/random_pic.php
@@ -3,7 +3,7 @@
 // 初始化随机数生成器种子
 $seed = time();
 // 获取随机数
-$num = rand(1, 2);
+$num = rand(1, 6);
 // 拼接图片地址
 $picpach = "https://cdn.jsdelivr.net/gh/IyaoiHarumi/Ichigo_cover/random_cover/".$num.".jpg";
 // 重定位到图片
